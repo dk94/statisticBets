e4d7556897f4daf1ed82a60b49f7f3aa3ce3c2b7
Dmytro <dmytro.kiyashko@outsourcing.com>
2017-09-15 10:10:39 +0000

    Few queries to DB implemented

--- connection.js
+++ connection.js
@@ -2,13 +2,6 @@ var MongoClient = require('mongodb').MongoClient;
 var assert = require('assert');
 var url = "mongodb://localhost:27017/mydb";
 
-module.exports = function connection() {
-    MongoClient.connect(url, function (err, db) {
-        if (err) throw err;
-        db.createCollection("customers", function (err, res) {
-            if (err) throw err;
-            console.log("Collection created!");
-            db.close();
-        });
-    });
+module.exports = function connection(callback) {
+    MongoClient.connect(url, callback);
 }
--- insertDataToDb.js
+++ insertDataToDb.js
@@ -1,5 +1,10 @@
 const connection = require('./connection');
 
-module.exports = function insertData(){
-    var newConnection = connection();
+module.exports = function insertData(data){
+    var newConnection = connection(function (err, db) {
+        if (err) throw err;
+        var cust = db.collection("customers2");
+        cust.insert(data)
+            .then((result)=>console.log(result));
+    });
 }
